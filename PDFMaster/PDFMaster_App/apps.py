a584ddf9269c7b5be0809386431f980d610f2ebf
jeelnathani <jeelnathani3@gmail.com>
2021-10-27 11:42:25 +0530

    Django backend added

---
+++ PDFMaster/PDFMaster_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PdfmasterAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'PDFMaster_App'
---
+++ PDFMaster/static/script/compress-script.js
@@ -0,0 +1,19 @@
+$(document).ready(
+    function() {
+        $('input:file').change(
+            function() {
+                if ($(this).val()) {
+                    $('button:submit').attr('disabled', false);
+                    // or, as has been pointed out elsewhere:
+                    // $('input:submit').removeAttr('disabled');
+                } else {
+                    $('button:submit').attr('disabled', true);
+                }
+            }
+        );
+    });
+
+//$('#compress-btn').click(function() {
+    //window.location.href = "{% url 'compress_download' %}";
+    //return false;
+//});
